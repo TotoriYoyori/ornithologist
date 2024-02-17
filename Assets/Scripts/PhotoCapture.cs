49975641ee43052924e50efd503f0ae7db5a18ee
gabriele.stasi <gabriele.stasi.7903@student.uu.se>
2024-02-17 17:51:58 +0100

    polaroid snapping and animation

---
+++ Assets/Scripts/PhotoCapture.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PhotoCapture : MonoBehaviour
+{
+    [Header("Photo Taker")]
+    [SerializeField] private Image photoDisplayArea;
+    [SerializeField] private GameObject photoFrame;
+
+    /*
+    [Header("Flash Effect")]
+    [SerializeField] private GameObject cameraFlash;
+    [SerializeField] private float flashTime;
+    */
+
+    [Header("Photo Fader Effect")]
+    [SerializeField] private Animator fadeInAnimation;
+
+    private Texture2D screenCapture;
+    private bool viewingPhoto;
+
+    private void Start()
+    {
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!viewingPhoto)
+            {
+                StartCoroutine(CapturePhoto());
+            }
+            else
+            {
+                RemovePhoto();
+            }
+        }
+    }
+
+    IEnumerator CapturePhoto()
+    {
+        viewingPhoto = true;
+
+        yield return new WaitForEndOfFrame();
+
+        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
+
+        screenCapture.ReadPixels(regionToRead, 0, 0, false);
+        screenCapture.Apply();
+        ShowPhoto();
+    }
+
+    void ShowPhoto()
+    {
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        photoDisplayArea.sprite = photoSprite;
+
+        photoFrame.SetActive(true);
+        //StartCoroutine(CameraFlashEffect());
+        fadeInAnimation.Play("PhotoFade");
+    }
+
+    /*
+     IEnumerator CameraFlashEffect()
+    {
+        cameraFlash.SetActive(true);
+        yield return new WaitForSeconds(flashTime);
+        cameraFlash.SetActive(false);
+    }
+    */
+    void RemovePhoto()
+    {
+        viewingPhoto = false;
+        photoFrame.SetActive(false);
+
+    }
+}
