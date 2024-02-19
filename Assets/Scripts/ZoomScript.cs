015eb25d50ad4333c585341b7f61bc9cf2dac278
gabriele.stasi <gabriele.stasi.7903@student.uu.se>
2024-02-19 20:26:51 +0100

    added zoom feature

---
+++ Assets/Scripts/ZoomScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class ZoomOnClick : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float zoomFactor = 2f; // Factor by which to zoom in
+
+    private bool isZoomed = false; // Flag to track whether the camera is currently zoomed in
+
+    private void Update()
+    {
+        // Check for right mouse button press
+        if (Input.GetMouseButtonDown(1)) // 1 corresponds to the right mouse button
+        {
+            if (!isZoomed)
+            {
+                // Get the position of the cursor in screen coordinates
+                Vector3 cursorPosition = Input.mousePosition;
+
+                // Convert screen coordinates to world coordinates at the depth of the camera
+                Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3(cursorPosition.x, cursorPosition.y, mainCamera.nearClipPlane));
+
+                // Zoom in on the area around the cursor position
+                mainCamera.transform.position = new Vector3(worldPosition.x, worldPosition.y, mainCamera.transform.position.z);
+                mainCamera.orthographicSize /= zoomFactor; // Zoom in by reducing the orthographic size
+
+                isZoomed = true;
+            }
+            else
+            {
+                // Reset the camera to its original state (normal zoom level)
+                mainCamera.transform.position = new Vector3(0f, 0f, mainCamera.transform.position.z);
+                mainCamera.orthographicSize *= zoomFactor; // Reset orthographic size to original value
+
+                isZoomed = false;
+            }
+        }
+    }
+}
