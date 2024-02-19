a587860e8f5a313586e351ec4ccd3f23f8a8107c
gabriele.stasi <gabriele.stasi.7903@student.uu.se>
2024-02-19 20:03:49 +0100

    photo storing bug solved

--- Assets/Scripts/PhotoCapture.cs
+++ Assets/Scripts/PhotoCapture.cs
@@ -17,12 +17,15 @@ public class PhotoCapture : MonoBehaviour
     [SerializeField] private Animator fadeInAnimation;
 
     private Texture2D screenCapture;
+    private Texture2D storedPhotoTexture;
     private bool viewingPhoto;
     private int storedPhotosCount = 0; // Count of currently stored photos
 
     private void Start()
     {
         screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        storedPhotoTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
     }
 
     private void Update()
@@ -35,7 +38,7 @@ public class PhotoCapture : MonoBehaviour
             }
             else
             {
-                StorePhoto();
+                StartCoroutine(CaptureStoredPhoto());
                 RemovePhoto();
             }
         }
@@ -53,6 +56,21 @@ public class PhotoCapture : MonoBehaviour
         screenCapture.Apply();
         ShowPhoto();
     }
+    IEnumerator CaptureStoredPhoto()
+    {
+        viewingPhoto = true;
+
+        yield return new WaitForEndOfFrame();
+
+        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
+
+        // Create a new Texture2D instance for the stored photo
+        Texture2D storedPhotoTexture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+        storedPhotoTexture.ReadPixels(regionToRead, 0, 0, false);
+        storedPhotoTexture.Apply();
+        StorePhoto(storedPhotoTexture); // Pass the captured texture to the StorePhoto method
+    }
 
     void ShowPhoto()
     {
@@ -62,15 +80,20 @@ public class PhotoCapture : MonoBehaviour
 
         fadeInAnimation.Play("PhotoFade");
     }
-
-    void StorePhoto()
+    void StorePhoto(Texture2D photoTexture)
     {
-        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
         if (storedPhotosCount < maxStoredPhotos)
         {
+            // Create a sprite from the new texture
+            Sprite photoSprite = Sprite.Create(photoTexture, new Rect(0.0f, 0.0f, photoTexture.width, photoTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+
+            // Assign the sprite to the current stored photo display area
             storedPhotoDisplayAreas[storedPhotosCount].sprite = photoSprite;
+
+            // Activate the corresponding stored photo frame
             storedPhotoFrames[storedPhotosCount].SetActive(true);
 
+            // Increment the count of stored photos
             storedPhotosCount++;
         }
         else
@@ -86,5 +109,6 @@ public class PhotoCapture : MonoBehaviour
 
         // Hide current photo frame
         photoDisplayArea.sprite = null;
+
     }
 }
